98ae6280d1085e80864b27cd29b916922ac450dc
Nawin Raj Kumra <nrknawirnajkumar888@gmail.com>
2023-07-24 20:51:28 +0530

    Added python inference file

---
+++ pipelines/inference.py
@@ -0,0 +1,15 @@
+from onnx_helper import ONNXClassifierWrapper
+import numpy as np
+
+BATCH_SIZE=64
+
+PRECISION = np.float32
+N_CLASSES = 1000 # Our ResNet-50 is trained on a 1000 class ImageNet task
+trt_model = ONNXClassifierWrapper("resnet_engine.trt", [BATCH_SIZE, N_CLASSES], target_dtype = PRECISION)
+
+# Generate a dummy batch.
+BATCH_SIZE = 32
+dummy_input_batch = np.zeros((BATCH_SIZE, 224, 224, 3), dtype = PRECISION)
+
+predictions = trt_model.predict(dummy_input_batch)
+
---
+++ pipelines/main.py
@@ -0,0 +1,12 @@
+# import libraries
+import torch
+import torchvision.models as models
+import torch.onnx
+
+# import the model
+resnext50_32x4d = models.resnext50_32x4d(pretrained=True)
+
+# Setting up Batch size
+BATCH_SIZE = 64
+dummy_input=torch.randn(BATCH_SIZE, 3, 224, 224)
+torch.onnx.export(resnext50_32x4d, dummy_input, "resnet50_onnx_model.onnx", verbose=False)
